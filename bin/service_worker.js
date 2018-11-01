7c80bbf3615f2e75db7e06c2cda0d0a181662722
Adrian Lee <alee.lcagency@gmail.com>
2018-11-01 17:36:49 +0800

    Complete Query route and modify update DB logic

--- bin/service_worker.js
+++ bin/service_worker.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
+const moment = require('moment');
 const { ConsoleLogger, RedisLogger, RedisClient } = require('../lib/shared/loader');
 const GatewayManager = require('../lib/payment_gateway');
 const Transaction = require('../lib/models/implementation/transaction');
@@ -23,25 +24,29 @@ class ServiceWorker {
         try {
             const task = await this.redis_client.popQueue(this.queue);
             if (!task) this.handleTask();
-            ConsoleLogger.log('info', 'Received task. Processing.....', '');
+            ConsoleLogger.log('info', 'Received task. Processing.....', task);
             const json_task = JSON.parse(task[1]);
-            const uuid = _.get(json_task, 'id');
-            const order = _.get(json_task, 'req.order');
-            const credit_card = _.get(json_task, 'req.credit_card');
+            const id = _.get(json_task, 'id');
+            const order = _.get(json_task, 'body.order');
+            const credit_card = _.get(json_task, 'body.credit_card');
             const gateway = _.get(json_task, 'gateway');
-            ConsoleLogger.log('info', 'Processed Data.  Call Gateway Service', `${gateway} ${uuid}: ${JSON.stringify(order)} ${JSON.stringify(credit_card)}`);
-            const result = await GatewayManager[gateway].call(uuid, order, credit_card);
+            ConsoleLogger.log('info', 'Processed Data.  Call Gateway Service', `${gateway} ${id}: ${JSON.stringify(order)} ${JSON.stringify(credit_card)}`);
+            const result = await GatewayManager[gateway].call(id, order, credit_card);
             const meta_status = _.get(result, 'meta');
             RedisLogger.log('info', 'Gateway called', result);
+            let res_payload;
+            let db_result;
             if (meta_status === 200) {
-                const res_payload = ServiceWorker.createPayload(uuid, order, 'Success');
-                await this.redis_client.writeCache(res_payload);
-                const newTransaction = new Transaction(res_payload);
-                await Transaction.record(newTransaction);
+                res_payload = ServiceWorker.createPayload(id, order, 'Success');
+                db_result = await Transaction.updateRecord(id, res_payload);
+                await this.redis_client.writeCache(db_result);
                 ConsoleLogger.log('info', 'Process succesful', '');
                 RedisLogger.log('info', 'Process Successfully Presist in Local', res_payload);
             } else {
-                ConsoleLogger.log('error', 'Processed failed', result);
+                res_payload = ServiceWorker.createPayload(id, order, 'Failed');
+                await this.redis_client.writeCache(res_payload);
+                await Transaction.updateRecord(id, res_payload);
+                ConsoleLogger.log('error', 'Processed failed at Payment Gateway', result);
             }
             this.handleTask();
         } catch (err) {
@@ -50,14 +55,15 @@ class ServiceWorker {
         }
     }
 
-    static createPayload(uuid, order, status) {
+    static createPayload(id, order, status) {
         return {
-            uuid,
+            id,
             status,
-            name: order.name,
-            phone_num: order.phone_num,
-            currency: order.currency,
-            price: order.price
+            name: _.get(order, 'name', ''),
+            phone_num: _.get(order, 'phone_num', ''),
+            currency: _.get(order, 'currency', ''),
+            price: _.get(order, 'price', 0),
+            updated_at: moment.utc().format('YYYY-MM-DDTHH:mm:ss.SSSZ')
         };
     }
 }
